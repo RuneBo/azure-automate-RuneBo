6aaf2855b4be34c91aedda6c8424bef98a808174
RuneBo <rune.bodding@nav.no>
2022-02-07 12:55:35 +0100

    oppgave-3

---
+++ src/oppgave-3.ps1
@@ -0,0 +1,24 @@
+
+
+$webRequest = Invoke-WebRequest -Uri http://nav-deckofcards.herokuapp.com/shuffle
+$kortstokk = $webRequest.Content | ConvertFrom-Json
+#write-output $kortstokk
+#foreach ( $kort in $kortstokk) {
+#    write-output "$($kort.suit[0])+$($kort.value)"
+#}
+function kortstokkTilStreng {
+    [OutputType([string])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+    $streng = ""
+    foreach ($kort in $kortstokk) {
+        $streng = $streng + "$($kort.suit[0])" + $($kort.value) + ","
+    }
+    return $streng = $streng.Substring(0,$streng.Length-1)
+}
+
+
+Write-Output "Kortstokk: $(kortStokkTilStreng -kortstokk $kortstokk)"
+
